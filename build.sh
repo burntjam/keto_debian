49a72ceec65370084999cba7fefc08e1198a040b
brett chaldecott <brett.chaldecott@gmail.com>
2018-09-25 08:06:40 +0000

    1) Fixed release process

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 function build {
-    if [ $# -ne 1 ]; then
-        echo "Must provide the path to the keto build"
+    if [ $# -ne 2 ]; then
+        echo "Must provide the path to the keto build and version $#"
         exit -1
     fi
 
     KETO_BUILD=$1
+    KETO_VERSION=$2
+    KETO_DATE=`date`
 
     mkdir -p keto/opt/keto/tmp
     mkdir -p keto/opt/keto/document_root
@@ -16,31 +18,42 @@ function build {
     mkdir -p keto/opt/keto/keys
     mkdir -p keto/opt/keto/shared
 
+    sed "s/VERSION_TAG/$KETO_VERSION/g" keto/resources/debian/control > keto/debian/control
+    sed "s/VERSION_TAG/$KETO_VERSION/g" keto/resources/debian/changelog > keto/debian/changelog
+    sed -i "s/DATE_TAG/$KETO_DATE/g" keto/debian/changelog
+
     cp -f $KETO_BUILD/build/install/bin/* keto/opt/keto/bin/.
     cp -f $KETO_BUILD/build/install/shared/* keto/opt/keto/shared/.
     cp -rf $KETO_BUILD/build/install/keys/* keto/opt/keto/keys/.
     cp -rf $KETO_BUILD/build/install/document_root/* keto/opt/keto/document_root/.
 
-    cd keto && fakeroot debian/rules binary
+    cd keto && fakeroot debian/rules binary && cd ../
+
+
+    cd keto/opt/keto/shared/ && tar -zcvf ../../../../keto_shared_$KETO_VERSION.tar.gz * && cd ../../../../
+    mkdir $KETO_VERSION
+    mv -f keto_${KETO_VERSION}_all.deb $KETO_VERSION
+    mv -f keto_shared_$KETO_VERSION.tar.gz $KETO_VERSION
+
+    s3cmd --recursive put $KETO_VERSION s3://keto-release/
 }
 
 
 function clean {
-    cd keto && debian/rules clean
+    cd keto && debian/rules clean && cd ..
     rm -rf keto/opt/keto/bin
     rm -rf keto/opt/keto/shared
     rm -rf keto/opt/keto/keys
     rm -rf keto/opt/keto/document_root
-    rm keto_*.deb
 }
 
 
 if [ "$1" ==  "build" ]; then
-    build $2
+    build $2 $3
 elif [ "$1" ==  "clean" ]; then
     clean
 else
     echo "Unrecognised parameters"
-    echo "\t./build.sh build <build path>"
-    echo "\t./build.sh clean"
+    echo "  ./build.sh build <build path> <version>"
+    echo "  ./build.sh clean"
 fi
